45e96f811111c7694f513cc0deb355978e7bdd70
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-23 12:14:23 -0300

    chore: config connection database

---
+++ src/config/database.ts
@@ -0,0 +1,20 @@
+import { getConnectionManager, ConnectionManager, Connection } from 'typeorm';
+import 'dotenv/config';
+
+const connectionManager: ConnectionManager = getConnectionManager();
+
+export const connection = async (): Promise<Connection> =>
+  connectionManager.create({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database:
+      process.env.NODE_ENV === 'test'
+        ? process.env.DB_NAME_TEST
+        : process.env.DB_NAME,
+  });
+
+export const close = (connection: Connection): Promise<void> =>
+  connection.close();
