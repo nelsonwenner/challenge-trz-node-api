1150201171b76c6c55732a4036049ade20618df9
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-22 23:20:29 -0300

    chore: add close in app

--- src/Server.ts
+++ src/Server.ts
@@ -1,9 +1,11 @@
 import './utils/module-alias';
 import Express, { Application } from 'express';
+import * as http from 'http';
 import cors from 'cors';
 
 export class Server {
   private readonly app: Application;
+  private server?: http.Server;
 
   constructor(private port = 3333) {
     this.app = Express();
@@ -18,13 +20,26 @@ export class Server {
     this.app.use(cors({ origin: '*' }));
   }
 
-  public start(): void {
-    this.app.listen(this.port, () => {
-      console.info(`\nServer start with successfully on PORT ${this.port}`);
-    });
-  }
-
   public getApp(): Application {
     return this.app;
+  }
+
+  public async close(): Promise<void> {
+    if (this.server) {
+      await new Promise((resolve, reject) => {
+        this.server?.close((err) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(null);
+        });
+      });
+    }
+  }
+
+  public start(): void {
+    this.server = this.app.listen(this.port, () => {
+      console.info(`\nServer start with successfully on PORT ${this.port}`);
+    });
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,20 @@ process.on('uncaughtException', (error) => {
     const server = new Server();
     await server.init();
     server.start();
+
+    const exitSignals: NodeJS.Signals[] = ['SIGINT', 'SIGTERM', 'SIGQUIT'];
+    for (const exitSignal of exitSignals) {
+      process.on(exitSignal, async () => {
+        try {
+          await server.close();
+          console.info(`App exited with success`);
+          process.exit(ExitStatus.Success);
+        } catch (error) {
+          console.error(`App exited with error: ${error}`);
+          process.exit(ExitStatus.Failure);
+        }
+      });
+    }
   } catch (error) {
     console.error(`App exited with error: ${error}`);
     process.exit(ExitStatus.Failure);
