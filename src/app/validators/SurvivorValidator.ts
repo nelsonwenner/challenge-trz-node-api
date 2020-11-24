99683e7ff6d7672fea5983738cfd30d33d05aabb
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-24 15:53:48 -0300

    feat: add SurvivorValidator

---
+++ src/app/validators/SurvivorValidator.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from 'express';
+import * as Yup from 'yup';
+
+export default async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<any> => {
+  try {
+    const schema = Yup.object()
+      .shape({
+        name: Yup.string().required(),
+        age: Yup.number().integer().positive().required(),
+        sex: Yup.string().required(),
+        inventory: Yup.array()
+          .of(
+            Yup.object()
+              .shape({
+                item: Yup.number().integer().positive().required(),
+                quantity: Yup.number().integer().required(),
+              })
+              .required()
+          )
+          .required(),
+        location: Yup.object()
+          .shape({
+            latitude: Yup.number().max(180).min(-180).required(),
+            longitude: Yup.number().max(180).min(-180).required(),
+          })
+          .required(),
+      })
+      .required();
+
+    await schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (error) {
+    switch (error.message) {
+      default:
+        return res
+          .status(400)
+          .json({ error: 'Validation fails', messages: error.inner });
+    }
+  }
+};
