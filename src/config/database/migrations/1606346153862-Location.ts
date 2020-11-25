9653ce6c8842efa4da638f4fe9615966fbc5b68f
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-25 20:43:56 -0300

    feat: add migrations

---
+++ src/config/database/migrations/1606346153862-Location.ts
@@ -0,0 +1,66 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class Location1606346153862 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'locations',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'latitude',
+            type: 'double',
+          },
+          {
+            name: 'longitude',
+            type: 'double',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+
+    await queryRunner.addColumn(
+      'locations',
+      new TableColumn({
+        name: 'survivorId',
+        type: 'varchar',
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'locations',
+      new TableForeignKey({
+        columnNames: ['survivorId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'survivors',
+        onDelete: 'CASCADE',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('locations');
+  }
+}
