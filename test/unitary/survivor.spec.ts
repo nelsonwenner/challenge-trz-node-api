eedffca33521bb992c26789ad173fb85ffce4ebf
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-25 15:03:38 -0300

    test: ensure return 400 if age does not is provided

--- test/unitary/survivor.spec.ts
+++ test/unitary/survivor.spec.ts
@@ -13,11 +13,33 @@ describe('Survivor unitary test', () => {
         longitude: longitude(),
       },
     };
+
     const res = await global.testRequest.post(prefix).send(reqFake);
+
     expect(res.status).toBe(400);
     expect(res.body).toEqual({
       code: 400,
       error: 'name is a required field',
+    });
+  });
+
+  test('Should return 400 if age does not is provided', async () => {
+    const reqFake = {
+      name: name(),
+      sex: sex(),
+      inventory: [item(1)],
+      location: {
+        latitude: latitude(),
+        longitude: longitude(),
+      },
+    };
+
+    const res = await global.testRequest.post(prefix).send(reqFake);
+
+    expect(res.status).toBe(400);
+    expect(res.body).toEqual({
+      code: 400,
+      error: 'age is a required field',
     });
   });
 });
