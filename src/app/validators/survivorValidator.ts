58f43f1d20ae630776fe605bb0930342db51a90f
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-25 16:24:31 -0300

    refactor: change format schema

--- src/app/validators/survivorValidator.ts
+++ src/app/validators/survivorValidator.ts
@@ -7,29 +7,21 @@ export default async (
   next: NextFunction
 ): Promise<any> => {
   try {
-    const schema = Yup.object()
-      .shape({
-        name: Yup.string().required(),
-        age: Yup.number().integer().positive().required(),
-        sex: Yup.string().required(),
-        inventory: Yup.array()
-          .of(
-            Yup.object()
-              .shape({
-                item: Yup.number().integer().positive().required(),
-                quantity: Yup.number().integer().required(),
-              })
-              .required()
-          )
-          .required(),
-        location: Yup.object()
-          .shape({
-            latitude: Yup.number().max(180).min(-180).required(),
-            longitude: Yup.number().max(180).min(-180).required(),
-          })
-          .required(),
-      })
-      .required();
+    const schema = Yup.object({
+      name: Yup.string().required(),
+      age: Yup.number().integer().positive().required(),
+      sex: Yup.string().required(),
+      inventory: Yup.array(
+        Yup.object({
+          item: Yup.number().integer().positive().required(),
+          quantity: Yup.number().integer().required(),
+        }).required()
+      ).required(),
+      location: Yup.object({
+        latitude: Yup.number().max(180).min(-180).required(),
+        longitude: Yup.number().max(180).min(-180).required(),
+      }).required(),
+    }).required();
 
     await schema.validate(req.body, { abortEarly: false });
 
