041c909da6597cb86ca0f28504f3ecb10a917a4a
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-25 23:25:31 -0300

    feat: add SurvivorRepository

---
+++ src/app/repositories/SurvivorRepository.ts
@@ -0,0 +1,17 @@
+import { SurvivorEntity } from '../models/Survivor';
+import { getRepository } from 'typeorm';
+
+interface SurvivorModel {
+  name: string;
+  age: number;
+  sex: string;
+}
+
+export class SurvivorRepository {
+  public static async create(data: SurvivorModel): Promise<SurvivorEntity> {
+    const survivorsRepository = getRepository(SurvivorEntity);
+    const survivor = survivorsRepository.create(data);
+    await survivorsRepository.save(survivor);
+    return survivor;
+  }
+}
