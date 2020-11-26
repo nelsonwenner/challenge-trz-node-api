1b8ee03131b93a7bf185b38ee3e67c373b52f1f8
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-26 16:09:16 -0300

    chore: add overrider in type on SyntaxError

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -9,17 +9,36 @@ const errorHandler: ErrorRequestHandler = (
   res: Response,
   next: NextFunction
 ): Response => {
+  if (err instanceof SyntaxError) {
+    return res.status(400).json({
+      code: 400,
+      error: {
+        body: err.body,
+        type: err.type,
+      },
+    });
+  }
+
   if (err instanceof ValidationError) {
-    return res.status(400).json({ code: 400, error: err.errors });
+    return res.status(400).json({
+      code: 400,
+      error: err.errors,
+    });
   }
 
   if (err instanceof AppError) {
-    return res.status(err.code).json({ code: err.code, error: err.error });
+    return res.status(err.code).json({
+      code: err.code,
+      error: err.error,
+    });
   }
 
   console.error(err);
 
-  return res.status(500).json({ code: 500, error: 'Internal server error.' });
+  return res.status(500).json({
+    code: 500,
+    error: 'Internal server error.',
+  });
 };
 
 export default errorHandler;
