11c824c3033d5149ee972f49e2c41e773a5ce156
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-27 17:01:34 -0300

    feat: add migration

---
+++ src/config/database/migrations/1606506602571-Inventory.ts
@@ -0,0 +1,60 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
+
+export class Inventory1606506602571 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'repositories',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    );
+
+    await queryRunner.addColumn(
+      'inventories',
+      new TableColumn({
+        name: 'survivorId',
+        type: 'varchar',
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      'inventories',
+      new TableForeignKey({
+        columnNames: ['survivorId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'survivors',
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('inventories', 'survivorId');
+    await queryRunner.dropColumn('inventories', 'survivorId');
+  }
+}
