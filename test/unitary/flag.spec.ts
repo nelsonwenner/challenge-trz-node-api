35588e62f4a6cdb53567241322112f8a1cadc7aa
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-30 16:30:18 -0300

    test: ensure return 400 if senderId does not is provided

---
+++ test/unitary/flag.spec.ts
@@ -0,0 +1,21 @@
+import { createSurvivor } from '../mock/survivor';
+
+const prefix = '/flags';
+
+describe('Flag unitary test', () => {
+  describe('When create a flag', () => {
+    test('Should return 400 if senderId does not is provided', async () => {
+      const reqFake = {
+        targetId: '0c82ac00-6994-45b4-9b5a-891183f3c5ef',
+      };
+
+      const res = await global.testRequest.post(prefix).send(reqFake);
+
+      expect(res.status).toBe(400);
+      expect(res.body).toEqual({
+        code: 400,
+        error: ['senderId is a required field'],
+      });
+    });
+  });
+});
