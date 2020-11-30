9659c18465fd60453ed344e60846ce942092c8d0
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-29 22:29:07 -0300

    test: ensure return 400 if survivorId does not is provided

--- test/unitary/location.spec.ts
+++ test/unitary/location.spec.ts
@@ -4,6 +4,21 @@ const prefix = '/locations';
 
 describe('Location unitary test', () => {
   describe('When update a location', () => {
+    test('Should return 400 if survivorId does not is provided', async () => {
+      const reqFake = {
+        latitude: latitude(),
+        longitude: longitude(),
+      };
+
+      const res = await global.testRequest.put(prefix).send(reqFake);
+
+      expect(res.status).toBe(400);
+      expect(res.body).toEqual({
+        code: 400,
+        error: ['survivorId is a required field'],
+      });
+    });
+
     test('Should return 400 if latitude does not is provided', async () => {
       const reqFake = {
         survivorId: 'e8045711-d1b1-3341-78bf-5111269121ab',
