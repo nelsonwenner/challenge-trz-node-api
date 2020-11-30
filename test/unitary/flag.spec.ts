3b6f6e8d4c2bb26fc11e20a7572f576bb42fc824
wenner <nelsondiasdemedeiros@gmail.com>
2020-11-30 16:32:19 -0300

    test: ensure return 400 if targetId does not is provided

--- test/unitary/flag.spec.ts
+++ test/unitary/flag.spec.ts
@@ -17,5 +17,19 @@ describe('Flag unitary test', () => {
         error: ['senderId is a required field'],
       });
     });
+
+    test('Should return 400 if targetId does not is provided', async () => {
+      const reqFake = {
+        senderId: '0c82ac00-6994-45b4-9b5a-891183f3c5ef',
+      };
+
+      const res = await global.testRequest.post(prefix).send(reqFake);
+
+      expect(res.status).toBe(400);
+      expect(res.body).toEqual({
+        code: 400,
+        error: ['targetId is a required field'],
+      });
+    });
   });
 });
